93286b43589db4a84058a49d9901ade0a8aa7580
franiel <66392105+franiiel@users.noreply.github.com>
2025-10-03 17:52:18 -0300

    Agregando animacion al boton del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -56,6 +56,16 @@ export default function Formulario() {
       console.log("Reserva:", formData);
     }
   };
+  const [animar, setAnimar] = useState(false);
+
+  const handleClick = () => {
+    // activar animación
+    setAnimar(true);
+
+    // después de 500ms sacar la clase
+    setTimeout(() => setAnimar(false), 500);
+  
+  };
 
   return (
     <Container
@@ -143,7 +153,7 @@ export default function Formulario() {
               </Form.Control.Feedback>
             </Form.Group>
 
-            <Button variant="primary" type="submit" className="boton-car">
+            <Button variant="primary" type="submit" className={`boton-car ${animar ? "animar" : ""}`} onClick={handleClick}>
               Enviar
             </Button>
           </Form>
