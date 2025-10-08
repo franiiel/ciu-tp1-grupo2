7d6818a3a2e66cd7ef29cfb1c9c3517ecad5229e
Maiite18 <maitealuminep@gmail.com>
2025-10-08 12:25:47 -0300

    footer y readme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,13 @@ function Footer() {
                 <Row className="align-items-center text-center text-md-start">
                 {/* Contacto */}
                 <Col md={6} className="mb-3 mb-md-0">
-                    <p>© Luna & Granos Café. Todos los derechos reservados.</p>                    
+                <h5 className="footer-title">Luna & Granos Café</h5>
+                <p className="mb-1">📍 Av. Siempre Viva 123, Buenos Aires</p>
+                <p className="mb-0">
+                <FaEnvelope className="me-2" />
+                contacto@lunaygranos.com
+                </p>
                 </Col>
-
                 {/* Redes sociales */}
                 <Col md={6} className="text-center text-md-end">
                     <a href="https://instagram.com" className="footer-icon me-3">
@@ -24,6 +28,11 @@ function Footer() {
                     <a href="https://twitter.com" className="footer-icon me-3">
                     <FaTwitter />
                     </a>
+                </Col>
+                </Row>
+                <Row className="justify-content-center">
+                <Col md={6} className="text-center">
+                    <p>© Luna & Granos Café. Todos los derechos reservados.</p>                    
                 </Col>
                 </Row>
             </Container>
